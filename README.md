6d47aaab5a56684d609ab5a94ecfe9cfdbcd6c16
Greg Dostatni <greg@commandprompt.com>
2024-07-27 18:53:06 -0400

    Initial proof of concept

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# caretaker
-Experimental code, demonstrates code self healing
+# Readme
+
+This is a simple demonstration project. More like this is possible at this point.
+
+Importing this code will give a simple python script rudimentary way of fixing itself.
+This scripts registers itself as a global exception hook, retrieves the source code that failed, executes llm to fix the code, substitutes it back in and continues execution as if nothing happened.
+
+Simple, eh?
+
+# Installation
+
+## Ollama
+
+Need ollama installed together with 
+pip install ollama.
+
+Need to download an appropriate model and set it inside caretaker/config.py. While you are at it, feel free to improve on the prompt.
+
+
+
+
+# Issues and future work
+
+There are several issues with this code. Some (most probably) are fixable.
+
+1. Simple scripts for now, cannot handle injecting into an object. Actually, I think it can, I just messed up with the stack and need to retrieve it properly.
+
+2. It only modifies the code in memory, does not write it back out to the filesystem. 
+
+3. There needs to be a system to figure out which model from ollama to run, there may be prompts specific to models. We want this to figure out what to do even a year later, when models may be different. Could it download a model for itself and then run it?
+
+4. Catching it at global exception hook means the program is already dead. Restarting execution (if the failure happened in a loop, etc), is tricky.
+
+5. A lot of the above can be handled by introducing a decorator (coming next). That will allow me to catch errors at a specific section of a code, store and retrieve different versions of related code from a database, catch, re-run and return code as if nothing happened, store things like specific prompts and intents. Kind of like an advanced error handler.
+
---
+++ caretaker/master.py
@@ -0,0 +1,88 @@
+import inspect
+import traceback
+import caretaker.config as config
+import ollama
+import re
+import linecache
+
+DEBUG = False
+
+def retrieve_fn_code(frames, frame):
+    """Retrieves the source code of the function on the the stack.
+    Starts at lowest level and returns code of first function it finds 
+    of the same name. Returns empty sting if not found."""
+    
+
+    function_name = frames[-1][2]
+
+    function = None
+    for filename, line_number, function_name, _ in reversed(frames):
+        if function_name in frame.f_globals.keys():
+            function = frame.f_globals[function_name]
+            break
+    if function:
+        source_code = inspect.getsource(function)
+    else:
+        source_code = ''
+
+    return source_code
+
+
+def fix_function(orig_source_code, err_text):
+    "Generates a new version of the function."
+    prompt = config.prompt.format(source_code=orig_source_code, error_message=err_text)
+    response = ollama.chat(model=config.model, messages=[
+    {
+        'role':'user',
+        'content':prompt,
+    }, ])
+
+    output = response['message']['content']
+    start_marker="```python\n"
+    end_marker = "```"
+    pattern = re.compile(f'{start_marker}(.*?){end_marker}', re.DOTALL)
+    matches = pattern.findall(output)
+    if matches:
+        corrected = matches[0]
+        return corrected
+    else:
+        # Possibly try again with a fallback LLM? 
+        # We do not have to give up
+        return ''
+    
+def continueExec(frame, f_globals, f_locals):
+            
+    # Now re-run the code that failed
+    code_context = frame.f_code
+    lineno = frame.f_lineno
+    
+    lines = linecache.getlines(code_context.co_filename)
+
+    for i in range(lineno -1 , len(lines)):
+        exec(lines[i], f_globals, f_locals)
+
+
+def exc(exc_type, exc_value, exc_tb):
+    "Global exception hook"
+    # obtain environment information about our exception
+    stack = inspect.stack()
+    frames = traceback.extract_tb(exc_tb)
+    frame = exc_tb.tb_frame
+
+    source_code = retrieve_fn_code(frames, frame)
+    if DEBUG:
+        print("Catching exception")
+        print(f"{source_code} {exc_value}")
+    
+    corrected = fix_function(source_code, exc_value)
+    
+    # Inject code
+    if corrected:
+        # Retrieve the environment in which this original function existed
+        f_locals = exc_tb.tb_frame.f_locals
+        f_globals = exc_tb.tb_frame.f_globals
+        # Execute the new function definition within this environment
+        exec(corrected, f_globals, f_locals)
+
+        # Continue execution (kinda)
+        continueExec(frame,f_globals, f_locals)
