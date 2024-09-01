813a954e6c275b9e2b3241e9acd3d0f24ded9703
greg_d128 <gregdostatni@gmail.com>
2024-09-01 15:51:13 -0400

    trying to make sense for how  models and multiple different mechanism for executing them should work

--- caretaker/ai/__init__.py
+++ caretaker/ai/__init__.py
@@ -3,34 +3,54 @@
 # known ai modules. We cannot assume that modules are actually 
 # available until we try to run them.
 
-# Need a common API.
+# This represents a common API used inside this code.
+# 
 # list_models() - list downloaded and available models
 # rewrite_code(model, question)
 # available() # models available for download
 # download
 
-models={}
+## Lets start with functionality we need.
+import logging
 
-def scan():
-    global models
-    models = {}
+logger = logging.getLogger("ai")
+
+
+class Models:
+    def __init__(self):
+        self.models = {}
+
+    def register_model(self, model_id, model_function):
+        logger.debug(f"Registering model: {model_id}")
+        self.models[model_id] = model_function
+
+    def get_model(self, model_id):
+
+        ret = self.models.get(model_id, None)
+        if ret:
+            logger.debug(f"Successfully retrieved model_id: {model_id}")
+        else:
+            logger.debug(f"Unable to retrieve model_id {model_id}")
+        
+        return ret
     
-    try:
-        import ollama
-        # add models models
-        models.update({ (ollama, m['name']):m for m in ollama.list()['models']})
-    except:
-        pass
-    print (models)
+    def execute_prompt(self, model_id, prompt):
+        logger.debug(f"Executing prompt on model {model_id}\n{prompt}\n\n")
+        model = self.get_model(model_id)
+        if not model:
+            raise ValueError(f"Model with ID {model_id} not found.")
+        return model.execute(prompt)
+
+    def list_models(self):
+        return list(self.models.keys())
+
+    def remove_model(self, model_id):
+        if model_id not in self.models:
+            logger.error(f"Unable to remove a model. Model {model_id} is not present.")
+        del self.models[model_id]
+        logger.debug(f"Successfully removed model_id {model_id}")
 
 
-def list_models():
-    return [x[1] for x in models.keys()]
+models=Models()
 
 
-def rewrite_code(instruction):
-    # Implement this.
-
-
-
-scan()
